b9f93da6efd46b1bf7ab475435a07a8746b90e09
jgx <137034198@qq.com>
2024-06-02 22:31:16 +0800

    send sh

---
+++ send.sh
@@ -0,0 +1,2 @@
+#! /bin/bash 
+cast send 0x10F340b563e0aB6b9F5F1d2D7CA4B681236700E9 "mint(address,uint256)" 0x3EC161C02Cd5a49EE8657947DC99DA58D1259aA1 1000 --rpc-url https://go.getblock.io/d51b9998c91b42c281a5da0eea5567a0 --private-key 0x34513a4c3547035a6b6ece66ab2cf41da990e15f607c379f71d079a4b4862862
